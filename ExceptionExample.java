a193b501060c25147aba92302950d1c7f49c7e27
MSH <157653802+MSH-trojan@users.noreply.github.com>
2024-07-31 10:56:50 -0400

    Add files via upload

---
+++ ExceptionExample.java
@@ -0,0 +1,15 @@
+package message;
+
+public class ExceptionExample {
+	public static void main(String[] args) {
+		
+		String b[] = {"Alpha" , "Beta"};
+		
+		System.out.println(b[3]);
+		
+		
+	}
+	
+}
+
+
